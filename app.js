586738b84e0457488461022ee215eb0bace60922
Tony Thompson <penguinprofession@icloud.com>
2019-04-07 19:23:18 -0400

    created routes to pages (corrected ext)

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 
 myWeatherApp = angular.module("myWeatherApp", ["ngRoute", "ngResource"]);
 
+// Angular Routes
+myWeatherApp.config(function ($routeProvider) {
+    $routeProvider
+
+    .when("/", {
+        templateUrl: "pages/main.htm",
+        controller: "mainController"
+    })
+
+    .when("/weather", {
+        templateUrl: "pages/weather.html",
+        controller: "weatherController"
+    })
+
+});
+
 // Angular Controllers
 
 myWeatherApp.controller("mainController", ["$scope", function($scope){
