352aeedcb50cdf000a1a1baa95d2971e8eac5d0f
Tony Thompson <penguinprofession@icloud.com>
2019-04-07 19:15:47 -0400

    created separate pages and controllers for each

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // Angular Module 
 
 myWeatherApp = angular.module("myWeatherApp", ["ngRoute", "ngResource"]);
+
+// Angular Controllers
+
+myWeatherApp.controller("mainController", ["$scope", function($scope){
+
+}]);
+
+myWeatherApp.controller("weatherController", ["$scope", function ($scope) {
+
+}]);
