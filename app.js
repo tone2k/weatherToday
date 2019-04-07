e6941febc3f0f20dfda5e778a9e3f8c5949bce4e
Tony Thompson <penguinprofession@icloud.com>
2019-04-07 19:49:06 -0400

    added watch method to scope for listening on city value

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ myWeatherApp.controller("mainController", ["$scope", "$cityService", function($s
 
     $scope.city = cityService.city
 
+    $scope.watch("city", function () {
+        cityService.city = $scope.city;
+    });
+
 }]);
 
 myWeatherApp.controller("weatherController", ["$scope", "cityService", function ($scope, $cityService) {
