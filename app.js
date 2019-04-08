33ac5fbe1bce511b4d2abffddede242c7ee17e98
Tony Thompson <penguinprofession@icloud.com>
2019-04-07 20:28:04 -0400

    troubleshooting openWeather API

--- app.js
+++ app.js
@@ -36,8 +36,27 @@ myWeatherApp.controller("mainController", ["$scope", "cityService", function($sc
 
 }]);
 
-myWeatherApp.controller("weatherController", ["$scope", "cityService", function ($scope, cityService) {
+myWeatherApp.controller("weatherController", ["$scope", "cityService", "$resource", function ($scope, cityService, $resource) {
 
     $scope.city = cityService.city
 
+    $scope.days = $routeParams.days || '2';
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK"
+    }, {
+        get: {
+            method: "JSONP"
+        }
+    });
+    $scope.weatherResult = $scope.weatherAPI.get({
+        zip: "22202,us",
+        cnt: $scope.days,
+        APPID: "8375f3c8e599fdb34d6658220af8d8c3"
+    });
+    $scope.convertToFahrenheit = function (degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    }
+    $scope.convertToDate = function (dt) {
+        return new Date(dt * 1000);
+    };
 }]);
